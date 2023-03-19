30171596242d7af94a82217d9cb30e859e0b4663
onurozcelik <muhendis.onur@gmail.com>
2023-03-19 14:52:02 +0300

    Convert Spring validation to ApiError

--- src/main/java/net/onurozcelik/hoaxify/user/UserController.java
+++ src/main/java/net/onurozcelik/hoaxify/user/UserController.java
@@ -8,8 +8,12 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.onurozcelik.hoaxify.shared.*;
@@ -22,22 +26,38 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/api/1.0/users")
-    public ResponseEntity<?> createUser(@Valid @RequestBody User user) {
-        String username = user.getUsername();
-        String displayName = user.getDisplayName();
+    public GenericResponse createUser(@Valid @RequestBody User user) {
+        /*
+         * We do not need manual validations after adding exception handler
+         * String username = user.getUsername();
+         * String displayName = user.getDisplayName();
+         * ApiError error = new ApiError(400, "Validation error", "api/1.0/users");
+         * Map<String, String> validationErrors = new HashMap<>();
+         * if (username == null || username.isEmpty()) {
+         * validationErrors.put("username", "Username cannot be null or empty");
+         * }
+         * if (displayName == null || displayName.isEmpty()) {
+         * validationErrors.put("displayName", "Display name cannot be null or empty");
+         * }
+         * error.setValidations(validationErrors);
+         * if (!validationErrors.isEmpty())
+         * return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+         * 
+         * userService.save(user);
+         * return ResponseEntity.ok(new GenericResponse("User created"));
+         */
+        return new GenericResponse("User created");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleValidationException(MethodArgumentNotValidException exception) {
         ApiError error = new ApiError(400, "Validation error", "api/1.0/users");
         Map<String, String> validationErrors = new HashMap<>();
-        if (username == null || username.isEmpty()) {
-            validationErrors.put("username", "Username cannot be null or empty");
-        }
-        if (displayName == null || displayName.isEmpty()) {
-            validationErrors.put("displayName", "Display name cannot be null or empty");
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
         error.setValidations(validationErrors);
-        if (!validationErrors.isEmpty())
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
-
-        userService.save(user);
-        return ResponseEntity.ok(new GenericResponse("User created"));
+        return error;
     }
 }
