3f33109a7d7a9fc8e94f8cbffedf44ca94f4fcbd
Onur Özçelik <muhendis.onur@gmail.com>
2023-03-23 13:02:28 +0300

    Implemented basic authentication

--- src/main/java/net/onurozcelik/hoaxify/error/ApiError.java
+++ src/main/java/net/onurozcelik/hoaxify/error/ApiError.java
@@ -3,17 +3,20 @@ package net.onurozcelik.hoaxify.error;
 import java.util.Date;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Data;
 
 import java.util.HashMap;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiError {
     private int status;
     private String message;
     private String path;
     private long timestamp = new Date().getTime();
-    private Map<String, String> validations = new HashMap<>();
+    private Map<String, String> validations;
 
     public ApiError(int status, String message, String path) {
         this.status = status;
