ebbf0c68b965cfd6850ae5e8ac7ca5bba2c0c8a1
Onur Özçelik <onur.ozcelik@rute.gov.tr>
2023-03-23 08:05:37 +0300

    Update application class to create initial users at startup

--- src/main/java/net/onurozcelik/hoaxify/HoaxifyApplication.java
+++ src/main/java/net/onurozcelik/hoaxify/HoaxifyApplication.java
@@ -1,14 +1,36 @@
 package net.onurozcelik.hoaxify;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import net.onurozcelik.hoaxify.user.*;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HoaxifyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HoaxifyApplication.class, args);
+	}
+	
+	// Spring injects dependencies automatically to beans
+	@Bean
+	CommandLineRunner createInitialUsers(UserService userService) {
+		return new CommandLineRunner() {
+
+			@Override
+			public void run(String... args) throws Exception {
+				User user = new User();
+				user.setUsername("user1");
+				user.setDisplayName("display1");
+				user.setPassword("P4ssword");
+				
+				userService.save(user);
+			}
+			
+		};
 	}
 
 }
