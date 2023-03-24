6a56b779decd67007b6027868a263b6aa5a7e8af
onurozcelik <muhendis.onur@gmail.com>
2023-03-25 02:48:27 +0300

    Update ErrorHandler to handle all errors, cleaned UserController

--- src/main/java/net/onurozcelik/hoaxify/error/ErrorHandler.java
+++ src/main/java/net/onurozcelik/hoaxify/error/ErrorHandler.java
@@ -1,11 +1,17 @@
 package net.onurozcelik.hoaxify.error;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.error.ErrorAttributeOptions.Include;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
@@ -19,11 +25,20 @@ public class ErrorHandler implements ErrorController {
     @RequestMapping("/error")
     ApiError handleError(WebRequest request) {
         Map<String, Object> attributes = this.errorAttributes.getErrorAttributes(request,
-                ErrorAttributeOptions.defaults());
-        String message = (String) attributes.get("error");
+                ErrorAttributeOptions.of(Include.MESSAGE, Include.BINDING_ERRORS));
+        String message = (String) attributes.get("message");
         String path = (String) attributes.get("path");
         int status = (int) attributes.get("status");
-        return new ApiError(status, message, path);
+        ApiError error = new ApiError(status, message, path);
+        if (attributes.containsKey("errors")) {
+            List<FieldError> fieldErrors = (List<FieldError>) attributes.get("errors");
+            Map<String, String> validationErrors = new HashMap<>();
+            for (FieldError fieldError : fieldErrors) {
+                validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+            error.setValidations(validationErrors);
+        }
+        return error;
     }
 
     public String getErrorPath() {
--- src/main/java/net/onurozcelik/hoaxify/user/UserController.java
+++ src/main/java/net/onurozcelik/hoaxify/user/UserController.java
@@ -1,23 +1,13 @@
 package net.onurozcelik.hoaxify.user;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.onurozcelik.hoaxify.shared.*;
-import net.onurozcelik.hoaxify.error.ApiError;
 
 @RestController
 public class UserController {
@@ -27,38 +17,7 @@ public class UserController {
 
     @PostMapping("/api/1.0/users")
     public GenericResponse createUser(@Valid @RequestBody User user) {
-        /*
-         * We do not need manual validations after adding exception handler
-         * String username = user.getUsername();
-         * String displayName = user.getDisplayName();
-         * ApiError error = new ApiError(400, "Validation error", "api/1.0/users");
-         * Map<String, String> validationErrors = new HashMap<>();
-         * if (username == null || username.isEmpty()) {
-         * validationErrors.put("username", "Username cannot be null or empty");
-         * }
-         * if (displayName == null || displayName.isEmpty()) {
-         * validationErrors.put("displayName", "Display name cannot be null or empty");
-         * }
-         * error.setValidations(validationErrors);
-         * if (!validationErrors.isEmpty())
-         * return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
-         * 
-         * userService.save(user);
-         * return ResponseEntity.ok(new GenericResponse("User created"));
-         */
         userService.save(user);
         return new GenericResponse("User created");
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleValidationException(MethodArgumentNotValidException exception) {
-        ApiError error = new ApiError(400, "Validation error", "api/1.0/users");
-        Map<String, String> validationErrors = new HashMap<>();
-        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
-            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
-        }
-        error.setValidations(validationErrors);
-        return error;
     }
 }
